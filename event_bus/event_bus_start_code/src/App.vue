3be925b83b253e99aeaa17369f1250e311de8b53
TJ <tjpataria@TJs-MacBook-Pro.local>
2020-10-14 09:38:31 +0100

    first commit

---
+++ event_bus/event_bus_start_code/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>Munros</h1>
+    <div class="main-container">
+      <munros-list :munros='munros'></munros-list>
+    </div>
+  </div>
+</template>
+
+<script>
+import MunrosList from './components/MunrosList.vue';
+
+export default {
+  name: 'app',
+  data(){
+    return {
+      munros: [],
+      selectedMunro: null
+    };
+  },
+  mounted(){
+    fetch('https://munroapi.herokuapp.com/munros')
+    .then(res => res.json())
+    .then(munros => this.munros = munros)
+  },
+  components: {
+    "munros-list": MunrosList
+  }
+}
+</script>
+
+<style>
+  .main-container {
+    display: flex;
+    justify-content: space-between;
+  }
+</style>
---
+++ event_bus/event_bus_start_code/src/components/ListItem.vue
@@ -0,0 +1,13 @@
+<template lang="html">
+  <li>{{munro.name}}</li>
+</template>
+
+<script>
+export default {
+  name: 'list-item',
+  props: ['munro']
+}
+</script>
+
+<style lang="css" scoped>
+</style>
