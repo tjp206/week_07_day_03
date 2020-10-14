bf11e63c2afb51e40356e56450b77ebb953e728d
TJ <tjpataria@TJs-MacBook-Pro.local>
2020-10-14 15:01:49 +0100

    learning to set up tests

---
+++ testing/testing_start/tests/e2e/specs/app.spec.js
@@ -0,0 +1,13 @@
+describe('App', () => {
+  beforeEach(() => {
+  })
+
+  it('Should have populated select', () => {
+  })
+
+  it('should show selected country on select change', () => {
+      })
+
+  xit('should add country to favourties on button click', () => {
+  })
+})
